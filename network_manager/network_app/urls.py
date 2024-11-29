0cabb330f76c526b4c9ed80e94ecceb1f941b8b7
chovy <feichovy202221190050@hotmail.com>
2024-11-29 14:15:04 +0000

    ca

--- network_manager/network_app/urls.py
+++ network_manager/network_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('config_device/', views.config_device, name='config_device'),
+    path('config_ospf/', views.config_ospf, name='config_ospf'),
+    path('config_ipsec/', views.config_ipsec, name='config_ipsec'),
+    path('config_acl/', views.config_acl, name='config_acl'),
 ]
