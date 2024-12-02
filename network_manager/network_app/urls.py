69216932d2721e5f81e3aa0990265ed2d1ccb803
chovy <feichovy202221190050@hotmail.com>
2024-12-02 15:21:34 +0000

    ad

--- network_manager/network_app/urls.py
+++ network_manager/network_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='home'),  # Welcome 主页面
+    path('', views.welcome, name='home'),  # 根路径指向 welcome 视图
     path('config_device/', views.config_device, name='config_device'),
     path('config_ospf/', views.config_ospf, name='config_ospf'),
     path('config_ipsec/', views.config_ipsec, name='config_ipsec'),
